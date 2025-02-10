f86feb3e61370f44a150838c47b263da7ebaf348
AyushAggarwal0331 <ayush0331.be21@chitkara.edu.in>
2025-02-10 22:03:37 +0530

    4th Lecture

---
+++ L4_100225/index.js
@@ -0,0 +1,52 @@
+// console.log("start");
+
+// var a=10;
+// function squareOfTwoNumber(x){
+//     var ans =x*x;
+//     return ans;
+// }
+
+// console.log(squareOfTwoNumber(a));
+// console.log("end");
+
+
+console.log(abc);
+sumOfTwo(8,4);
+var abc=29;
+function sumOfTwo(a,b){
+    var ans=a+b;
+    return ans;
+}
+var majorAns = sumOfTwo(10,5);
+console.log(majorAns);
+
+
+
+// console.log(xyz);
+// console.log(sumOfTwo(10,16));
+// let xyz=12;
+// function sumOfTwo(a,b){
+//     let ans=a+b;
+//     return ans;
+
+// }
+
+// console.log(sumOfTwo(19,27));
+
+
+var a=20;
+var b=30;
+var c=40;
+{
+    var a=5;
+    var b=4;
+    var c=18;
+    {
+        var a=87;
+        var b=23;
+        var x=43;
+        console.log(a);
+        console.log(b);
+        console.log(c);
+    }
+}
