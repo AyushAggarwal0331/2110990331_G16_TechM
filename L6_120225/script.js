09b2b6b6b7b5e214634b0076ab00632f454cb438
AyushAggarwal0331 <ayush0331.be21@chitkara.edu.in>
2025-02-12 15:38:09 +0530

    Lecture6

---
+++ L6_120225/script.js
@@ -0,0 +1,42 @@
+// console.log(1)
+// console.log(SumOfTwo(10,5));
+
+// function SumOfTwo(a,b){
+//     return a+b;
+// }
+// setTimeout(() => {
+//     console.log(4)
+// }, 2000);
+
+var start =1;
+var end =100000;
+
+// while(start<end){
+//     start++;
+// }
+// console.log("end");
+
+// setInterval(() => {
+//     console.log(9);
+// }, 4000);
+
+
+// FUNCTIONS
+
+// 1. named function
+xyz(10,5);
+function xyz(a,b){
+    console.log(a+b);
+}
+
+// 2. anonymous function
+var hello = function gg(){
+    console.log("hello");
+}
+hello()
+
+// 3. arrow function
+abc = ()=>{
+    console.log(2*2)
+}
+abc()
